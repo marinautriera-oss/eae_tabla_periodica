21024ec1203fd89639018bf16c22c0b2b6662cdc
marina <marina.utriera@gmail.com>
2026-06-27 10:37:08 -0300

    feat: cambios en el admin panel

--- src/services/elementService.js
+++ src/services/elementService.js
@@ -3,16 +3,38 @@ import { getAllElements, getElementById, createElement, updateElement, deleteEle
 export const getAll = async() => {
     return await getAllElements();
 };
+
 export const getById = async (id) => {
     return await getElementById(id);
 };
 
 export const create = async (data) => {
-    return await createElement(data.NOMBRE, data.SIMBOLO, data.NUMERO_ATOMICO, data.MASA_ATOMICA, data.GRUPO, data.PERIODO, data.CATEGORIA, data.FOTO_URL, data.DESCRIPCION);
+    return await createElement(
+        data.NOMBRE,
+        data.SIMBOLO,
+        data.NUMERO_ATOMICO,
+        data.MASA_ATOMICA,
+        data.GRUPO,
+        data.PERIODO,
+        data.CATEGORIA,
+        data.FOTO_URL || null,
+        data.DESCRIPCION || null
+    );
 };
 
 export const update = async (id, data) => {
-    return await updateElement(id, data.NOMBRE, data.SIMBOLO, data.NUMERO_ATOMICO, data.MASA_ATOMICA, data.GRUPO, data.PERIODO, data.CATEGORIA, data.FOTO_URL, data.DESCRIPCION);
+    return await updateElement(
+        id,
+        data.NOMBRE,
+        data.SIMBOLO,
+        data.NUMERO_ATOMICO,
+        data.MASA_ATOMICA,
+        data.GRUPO,
+        data.PERIODO,
+        data.CATEGORIA,
+        data.FOTO_URL || null,
+        data.DESCRIPCION || null
+    );
 };
 
 export const remove = async (id) => {
